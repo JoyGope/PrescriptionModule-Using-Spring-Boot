2a6db26cf014bf56ed2756714d3847f103a7913c
JoyGope <joy.chandra.gope@g.bracu.ac.bd>
2024-04-22 11:58:58 +0600

    first commit

---
+++ src/main/java/com/cmed/cmed/PrescriptionHandlerApplication.java
@@ -0,0 +1,16 @@
+package com.cmed.cmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+
+public class PrescriptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionHandlerApplication.class, args);
+	}
+
+}
